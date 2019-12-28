d216758dfed4ef3810a754b248aadd6163c7c493
Peter Pshenichny <peter.pshenichny@gmail.com>
2019-12-28 17:39:10 +0200

    Added zip and file copy plugins to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const ZipPlugin = require('zip-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -18,5 +20,14 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CopyPlugin([
+      { from: './public', to: './' },
+    ]),
+    new ZipPlugin({
+      path: '../',
+      filename: 'birthday-calendar-exporter.zip'
+    })
+  ],
   mode: 'production'
 };
