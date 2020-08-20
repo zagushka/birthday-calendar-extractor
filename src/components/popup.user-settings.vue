b56a7e6d235db1903b8fd0f543b5d773c435d289
Peter Pshenichny <peter.pshenichny@gmail.com>
2020-08-20 22:50:08 +0300

    added preloader

--- src/components/popup.user-settings.vue
+++ src/components/popup.user-settings.vue
@@ -1,23 +1,35 @@
 <template>
   <span>
-    <p><strong v-translate="'USER_SETTINGS'"></strong></p>
-    <div class="no-wrap">
-      <input type="radio" id="format_ics" name="file_format" value="ics" v-model="file_format">
-      <label for="format_ics" v-translate="'SELECT_FILE_FORMAT_ICS'"></label>
+    <span :style.visibility="!waiting ? 'visible': 'hidden'">
+      <p><strong v-translate="'USER_SETTINGS'"></strong></p>
+      <div class="no-wrap">
+        <input type="radio" id="format_ics" name="file_format" value="ics" v-model="file_format">
+        <label for="format_ics" v-translate="'SELECT_FILE_FORMAT_ICS'"></label>
+      </div>
+      <div class="no-wrap">
+        <input type="radio" id="format_csv" name="file_format" value="csv" v-model="file_format">
+        <label for="format_csv" v-translate="'SELECT_FILE_FORMAT_CSV'"></label>
+      </div>
+      <div class="no-wrap">
+        <input type="radio" id="format_delete-ics" name="file_format" value="delete-ics" v-model="file_format">
+        <label for="format_delete-ics" v-translate="'SELECT_FILE_FORMAT_DELETE_ICS'"></label>
+      </div>
+      <br/>
+      <div class="no-wrap">
+        <a v-on:click="startGeneration()" class="link"><span v-translate="'GENERATE'"></span></a>
+        <a v-link="'LEAVE_FEEDBACK_LINK'" class="link special"><span v-translate="'LEAVE_FEEDBACK_TITLE'"></span></a>
+      </div>
+    </span>
+    <div id="spinner-container" v-show="waiting">
+      <div class="spinner">
+        <div class="rect1"></div>
+        <div class="rect2"></div>
+        <div class="rect3"></div>
+        <div class="rect4"></div>
+        <div class="rect5"></div>
+      </div>
     </div>
-    <div class="no-wrap">
-      <input type="radio" id="format_csv" name="file_format" value="csv" v-model="file_format">
-      <label for="format_csv" v-translate="'SELECT_FILE_FORMAT_CSV'"></label>
-    </div>
-    <div class="no-wrap">
-      <input type="radio" id="format_delete-ics" name="file_format" value="delete-ics" v-model="file_format">
-      <label for="format_delete-ics" v-translate="'SELECT_FILE_FORMAT_DELETE_ICS'"></label>
-    </div>
-    <br/>
-    <div class="no-wrap">
-      <a v-on:click="startGeneration()" class="link"><span v-translate="'GENERATE'"></span></a>
-      <a v-link="'LEAVE_FEEDBACK_LINK'" class="link special"><span v-translate="'LEAVE_FEEDBACK_TITLE'"></span></a>
-    </div>
+
   </span>
 </template>
 
@@ -54,11 +66,13 @@ const PopupUserSettings = Vue.extend({
   data: () => {
     return {
       file_format: 'ics',
+      waiting: false,
     };
   },
   methods: {
     startGeneration() {
       sendMessage(new StartGenerationAction());
+      this.waiting = true;
     },
   },
 });
@@ -66,8 +80,77 @@ const PopupUserSettings = Vue.extend({
 export default PopupUserSettings;
 </script>
 
-<style>
+<style type="scss">
 .no-wrap {
   white-space: nowrap;
 }
+
+#spinner-container {
+  background-color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1000;
+}
+
+.spinner {
+  margin: calc(50vh - 20px) auto;
+  width: 50px;
+  height: 40px;
+  text-align: center;
+  font-size: 10px;
+}
+
+.spinner > div {
+  background-color: #3b6e22;
+  height: 100%;
+  width: 6px;
+  display: inline-block;
+
+  -webkit-animation: sk-stretchdelay 1.2s infinite ease-in-out;
+  animation: sk-stretchdelay 1.2s infinite ease-in-out;
+}
+
+.spinner .rect2 {
+  -webkit-animation-delay: -1.1s;
+  animation-delay: -1.1s;
+}
+
+.spinner .rect3 {
+  -webkit-animation-delay: -1.0s;
+  animation-delay: -1.0s;
+}
+
+.spinner .rect4 {
+  -webkit-animation-delay: -0.9s;
+  animation-delay: -0.9s;
+}
+
+.spinner .rect5 {
+  -webkit-animation-delay: -0.8s;
+  animation-delay: -0.8s;
+}
+
+@-webkit-keyframes sk-stretchdelay {
+  0%, 40%, 100% {
+    -webkit-transform: scaleY(0.4)
+  }
+  20% {
+    -webkit-transform: scaleY(1.0)
+  }
+}
+
+@keyframes sk-stretchdelay {
+  0%, 40%, 100% {
+    transform: scaleY(0.4);
+    -webkit-transform: scaleY(0.4);
+  }
+  20% {
+    transform: scaleY(1.0);
+    -webkit-transform: scaleY(1.0);
+  }
+}
+
 </style>
