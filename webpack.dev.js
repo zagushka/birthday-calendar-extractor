336e968ff23e291bd3e28214ec777dc9fb3e95d3
Peter Pshenichny <peter.pshenichny@gmail.com>
2020-08-31 21:26:19 +0300

    fixed hmr

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,18 @@
 const ExtensionReloader = require('webpack-extension-reloader');
-const {merge, mergeWithCustomize, customizeArray} = require('webpack-merge');
+const {mergeWithCustomize, customizeArray} = require('webpack-merge');
 const common = require('./webpack.common.js');
+
+const plugins = [];
+if (process.env.HMR) {
+  plugins.push(new ExtensionReloader({
+    // entries: {
+    //   contentScript: 'content',
+    //   background: 'background',
+    //   popup: 'popup',
+    // }
+    reloadPage: true
+  }))
+}
 
 module.exports = mergeWithCustomize({
   customizeArray: customizeArray({
@@ -10,14 +22,5 @@ module.exports = mergeWithCustomize({
 (common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  plugins: [
-    new ExtensionReloader({
-      // entries: {
-      //   contentScript: 'content',
-      //   background: 'background',
-      //   popup: 'popup',
-      // }
-      reloadPage: true
-    }),
-  ],
+  plugins: plugins,
 });
