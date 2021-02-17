deec93c9137f7e2f25d75963e8e7689c08a95b63
Peter Pshenichny <peter.pshenichny@gmail.com>
2021-02-17 20:22:57 +0200

    removed error context

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,10 +1,8 @@
 import React, {
   FunctionComponent,
-  useContext,
   useEffect,
 } from 'react';
 import UserSettings from '../components/user-settings/user-settings';
-import ErrorsContextProvider, { ErrorsContext } from '../context/errors.context';
 import LoadingContextProvider from '../context/loading.context';
 import SettingsContextProvider from '../context/settings.context';
 import TodayUsersContextProvider from '../context/today-users.context';
@@ -13,34 +11,18 @@ import { sendMessage } from '../libs/events/events';
 import './App.scss';
 
 const App: FunctionComponent = () => {
-  const {error, resetError} = useContext(ErrorsContext);
 
   useEffect(() => {
     // Send notification badge was clicked
     sendMessage(badgeClickedAction(), true);
   }, []);
 
-  // const content = (() => {
-  //   switch (error) {
-  //     case 'DONE':
-  //       return <DoneModal onHide={() => resetError()}/>;
-  //     case 'NOT_SUPPORTED_LANGUAGE':
-  //       return <ChangeLanguageModal onHide={() => resetError()}/>;
-  //     case 'NO_TOKEN_DETECTED':
-  //       return <NoTokenDetectedModal onHide={() => resetError()}/>;
-  //     default:
-  //       return <UserSettings/>;
-  //   }
-  // })();
-
   return (
     <LoadingContextProvider>
       <SettingsContextProvider>
-        <ErrorsContextProvider>
-          <TodayUsersContextProvider>
-            <UserSettings/>
-          </TodayUsersContextProvider>
-        </ErrorsContextProvider>
+        <TodayUsersContextProvider>
+          <UserSettings/>
+        </TodayUsersContextProvider>
       </SettingsContextProvider>
     </LoadingContextProvider>
   );
