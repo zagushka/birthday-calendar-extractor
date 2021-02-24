aa208123b4532399aeef3592d4e28815ff77db9d
Peter Pshenichny <peter.pshenichny@gmail.com>
2021-02-24 12:57:48 +0200

    Executed script for birthdays scan

---
+++ src/libs/executed-script-scan.ts
@@ -0,0 +1,182 @@
+import {
+  ScanErrorTypes,
+  ExecutedScriptScanResponseAction,
+  RawScannedUser,
+} from './events/executed-script.types';
+
+export const fetchUserFriendsBirthdayInfoFromContext = (): string => {
+  const responseId = Math.random().toString();
+  sendScanLog('LOOKING_FOR_FACEBOOK_TOKEN');
+  /**
+   * Make request to facebook.com in order to receive html with vital information such as
+   * token
+   */
+  getFacebookPage()
+    .then(extractFacebookToken)
+    // Fetch user friends birthday data
+    .then(fetchFriendsBirthdayInfo)
+    .then(extractBirthdays)
+    .then((result) => executedScriptUserContextResponse(result))
+    .catch(error => executedScriptUserContextError(error.messageName, error.modal));
+
+  /**
+   * Send message with Scan log update
+   *
+   * @param messageName - message name to be used for i18n internalisation
+   * @param substitutions - optional substitutions for messageName
+   */
+  function sendScanLog(messageName: string, substitutions?: Array<string>) {
+    chrome.runtime.sendMessage({
+      type: 'SEND_SCAN_LOG',
+      payload: {messageName, substitutions},
+    });
+  }
+
+  /**
+   * Send message with an error, background and popup script will listen for `EXECUTED_SCRIPT_RESPONSE` event type
+   *
+   * @param messageName - error description to be used with i18n
+   * @param error - anything else describes the error
+   */
+  function executedScriptUserContextError(messageName: ScanErrorTypes, error?: any) {
+    const message: ExecutedScriptScanResponseAction = {
+      type: 'EXECUTED_SCRIPT_RESPONSE',
+      responseId,
+      payload: {
+        type: messageName ?? 'SCAN_ERROR_GENERAL',
+        error,
+      },
+    };
+    chrome.runtime.sendMessage(message);
+  }
+
+  /**
+   * Fake ExecutedScriptContextResponse since the function is not accessible from here
+   *
+   * @param users - Array of extracted user birthdays
+   */
+  function executedScriptUserContextResponse(users: Array<RawScannedUser>) {
+    const message: ExecutedScriptScanResponseAction = {
+      type: 'EXECUTED_SCRIPT_RESPONSE',
+      responseId,
+      payload: {
+        type: 'SCAN_SUCCESS',
+        users,
+      },
+    };
+    chrome.runtime.sendMessage(message);
+  }
+
+  /**
+   * Parse provided html page and extract token
+   * token will be found on the page inside json like construction "token":"TOKEN_VALUE"
+   *
+   * @param page - fetched facebook page
+   */
+  function extractTokenFromPage(page: string): string {
+    const pattern = new RegExp('.*"token":"(.*?)"', 'm');
+    const result = pattern.exec(page);
+    return result && result[1];
+  }
+
+  /**
+   * Fetch https://www.facebook.com page and handle related errors
+   */
+  async function getFacebookPage(): Promise<string> {
+    let response: Response;
+    try {
+      /**
+       * Make request to facebook.com in order to receive html with vital information such as
+       * token
+       */
+      response = await fetch('https://www.facebook.com', {headers: {accept: 'text/html'}});
+    } catch (error) {
+      return Promise.reject({messageName: 'SCAN_ERROR_FACEBOOK_PAGE_REQUEST', error});
+    }
+
+    try {
+      sendScanLog('EXTRACTING_FACEBOOK_TOKEN');
+      return await response.text();
+    } catch (error) {
+      return Promise.reject({messageName: 'SCAN_ERROR_FACEBOOK_PAGE_CONTENT', error});
+    }
+  }
+
+  /**
+   * Make graphQL request and page and handle response errors
+   *
+   * @param token - token to use with facebook requests
+   */
+  async function fetchFriendsBirthdayInfo(token: string) {
+    let response: Response;
+    try {
+      /**
+       * Make request to facebook.com in order to receive html with vital information such as
+       * token
+       */
+      sendScanLog('FETCHING_FRIENDS_BIRTHDAYS');
+      response = await fetch('https://www.facebook.com/api/graphql/', {
+        'headers': {
+          'content-type': 'application/x-www-form-urlencoded',
+        },
+        'body': '&__a=1'
+          + '&fb_dtsg=' + encodeURIComponent(token)
+          + '&fb_api_caller_class=RelayModern'
+          + '&fb_api_req_friendly_name=BirthdayCometMonthlyBirthdaysRefetchQuery'
+          + '&variables=' + encodeURIComponent(JSON.stringify({'count': 12}))
+          + '&server_timestamps=true'
+          + '&doc_id=3681233908586032',
+        'method': 'POST',
+      });
+    } catch (error) {
+      return Promise.reject({messageName: 'SCAN_ERROR_FACEBOOK_BIRTHDAYS_REQUEST', error});
+    }
+
+    try {
+      sendScanLog('EXTRACTING_FACEBOOK_BIRTHDAYS');
+      return await response.json();
+    } catch (error) {
+      return Promise.reject({messageName: 'SCAN_ERROR_FACEBOOK_BIRTHDAYS_CONTENT', error});
+    }
+  }
+
+  /**
+   * Extract facebook token from fetched facebook page, handle errors
+   *
+   * @param page - page content
+   */
+  async function extractFacebookToken(page: string) {
+    const token = extractTokenFromPage(page);
+    // No token found, report an error, quit
+    if ('string' !== typeof token) {
+      sendScanLog('SCAN_LOG_CHECK_LOGIN_ERROR_LOGIN');
+      return Promise.reject({messageName: 'SCAN_ERROR_NO_TOKEN_DETECTED'});
+    }
+    sendScanLog('SCAN_LOG_FACEBOOK_TOKEN_FOUND');
+    return token;
+  }
+
+  /**
+   * Extract user birthdays info from previously fetched data
+   *
+   * @param response - previously fetched json with user birthday data
+   */
+  async function extractBirthdays(response: any) {
+    sendScanLog('EXTRACTING_FACEBOOK_BIRTHDAYS');
+    try {
+      return response.data.viewer.all_friends_by_birthday_month.edges
+        .map(({node}: any) => node.friends.edges)
+        .flat()
+        .map(({node}: any) => ({
+          name: node.name,
+          birthdate: node.birthdate,
+          id: node.id,
+        }));
+    } catch (error) {
+      sendScanLog('FAILED_BIRTHDAYS_EXTRACT', [error]);
+      return Promise.reject({messageName: 'SCAN_ERROR_BIRTHDAYS_EXTRACT', error});
+    }
+  }
+
+  return responseId;
+};
