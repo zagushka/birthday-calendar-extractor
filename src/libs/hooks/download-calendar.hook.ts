11427c072ef8adea95d2de685d07f91e80e0b956
Peter Pshenichny <peter.pshenichny@gmail.com>
2021-03-02 23:53:19 +0200

    Download calendar hook to handle download calendar process

---
+++ src/libs/hooks/download-calendar.hook.ts
@@ -0,0 +1,49 @@
+import {
+  useEffect,
+  useState,
+} from 'react';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { downloadCalendar } from '../download-calendar';
+import { StoredBirthday } from '../storage/storaged.types';
+
+export function useCalendarDownloader(calendarType: any, users: Array<StoredBirthday>, settings?: any) {
+  const [status, setStatus] = useState<'working' | 'standby'>('standby');
+  const [result, setResult] = useState<'success' | 'failure' | 'pending'>('pending');
+  const [error, setError] = useState<any>();
+
+  useEffect(() => {
+    if ('standby' === status) {
+      return;
+    }
+
+    const onDestroy$ = new Subject();
+    downloadCalendar(calendarType, users, settings)
+      .pipe(takeUntil(onDestroy$))
+      .subscribe({
+        next: () => {
+          setResult('success');
+          setStatus('standby');
+        },
+        error: (e) => {
+          setResult('failure');
+          setStatus('standby');
+          setError(e);
+        },
+      });
+
+    return () => {
+      onDestroy$.next(true);
+      onDestroy$.complete();
+      setResult('pending');
+    };
+
+  }, [status, calendarType, users, settings]);
+
+  return {
+    status,
+    result,
+    error,
+    startDownload: () => setStatus('working'),
+  };
+}
