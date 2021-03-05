3ba885f0415b06e79412265d8c9b91846b4f892a
Peter Pshenichny <peter.pshenichny@gmail.com>
2021-03-05 22:20:55 +0200

    new button with 'generate and download'

---
+++ src/components/buttons/generate.button/generate.button.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@material-ui/core';
+import React, { FunctionComponent } from 'react';
+import { translateString } from '../../../filters/translateString';
+
+interface GenerateAndDownloadButtonProps {
+  disabled?: boolean;
+  onClick: () => void;
+}
+
+const GenerateAndDownloadButton: FunctionComponent<GenerateAndDownloadButtonProps> = (props) => {
+  const {onClick, disabled = false} = props;
+  return (
+    <Button size='small'
+            variant='contained'
+            color='primary'
+            disabled={disabled}
+            onClick={onClick}>
+      {translateString('GENERATE')}
+    </Button>
+  );
+};
+
+export default GenerateAndDownloadButton;
