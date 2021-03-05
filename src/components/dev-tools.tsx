70aede3ee5f6f8c962316f2447704172d622d433
Peter Pshenichny <peter.pshenichny@gmail.com>
2021-03-05 21:25:25 +0200

    testing modals

--- src/components/dev-tools.tsx
+++ src/components/dev-tools.tsx
@@ -5,7 +5,21 @@ import {
   setBadgeColor,
   setBadgeText,
 } from '../libs/badge';
-import { clearStorage } from '../libs/storage/chrome.storage';
+import {
+  SCAN_ERROR_BIRTHDAYS_EXTRACT,
+  SCAN_ERROR_FACEBOOK_REQUIRED,
+  SCAN_ERROR_NO_TOKEN_DETECTED,
+  ScanErrorTypes,
+} from '../libs/events/executed-script.types';
+import {
+  SHOW_MODAL_EXPORT_SUCCESS,
+  SHOW_MODAL_SCANNING,
+  ShowModalTypes,
+} from '../libs/events/types';
+import {
+  clearStorage,
+  storeUserSettings,
+} from '../libs/storage/chrome.storage';
 import Layout from './layout/layout';
 
 const DevTools: React.FunctionComponent = () => {
@@ -18,15 +32,27 @@ const DevTools: React.FunctionComponent = () => {
     setBadgeColor();
   };
 
+  const openModal = (type: ScanErrorTypes | ShowModalTypes) => () => {
+    storeUserSettings({modal: {type}});
+  };
+
   return (
     <Layout.Wrapper>
+
       <Layout.Header>
         <Box>Dev Tools</Box>
       </Layout.Header>
+
       <Layout.Content>
-        <Button onClick={clearChromeStorage}>Clear Chrome Local Storage</Button>
-        <Button onClick={clearBadge}>Clear Badge</Button>
+        <Button variant='outlined' color='secondary' onClick={clearChromeStorage}>Clear Chrome Local Storage</Button>
+        <Button variant='outlined' onClick={clearBadge}>Clear Badge</Button>
+        <Button variant='outlined' onClick={openModal(SCAN_ERROR_BIRTHDAYS_EXTRACT)}>Birthdays Extract Failed Modal</Button>
+        <Button variant='outlined' onClick={openModal(SHOW_MODAL_SCANNING)}>Scanning Modal</Button>
+        <Button variant='outlined' onClick={openModal(SHOW_MODAL_EXPORT_SUCCESS)}>Export Success Modal</Button>
+        <Button variant='outlined' onClick={openModal(SCAN_ERROR_FACEBOOK_REQUIRED)}>Facebook Required Modal</Button>
+        <Button variant='outlined' onClick={openModal(SCAN_ERROR_NO_TOKEN_DETECTED)}>No token detected Modal</Button>
       </Layout.Content>
+
     </Layout.Wrapper>);
 };
 
