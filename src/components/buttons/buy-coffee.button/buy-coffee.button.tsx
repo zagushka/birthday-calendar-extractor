0f9ffc3e6d875d59f1539badc3e284b12500ea43
Peter Pshenichny <peter.pshenichny@gmail.com>
2021-03-07 19:25:00 +0200

    additional variations of the "buy me a coffee" button

--- src/components/buttons/buy-coffee.button/buy-coffee.button.tsx
+++ src/components/buttons/buy-coffee.button/buy-coffee.button.tsx
@@ -1,15 +1,51 @@
-import { Button } from '@material-ui/core';
-import React, { FunctionComponent } from 'react';
+import {
+  Button,
+  ButtonProps,
+  IconButton,
+  IconButtonProps,
+} from '@material-ui/core';
+import { FreeBreakfast } from '@material-ui/icons';
+import React, {
+  FunctionComponent,
+  useEffect,
+  useState,
+} from 'react';
 import handleLink from '../../../filters/handleLink';
 import { translateString } from '../../../filters/translateString';
 
-const BuyCoffeeButton: FunctionComponent = () => {
+interface BuyCoffeeButtonProps extends ButtonProps {
+  withIcon?: boolean;
+}
+
+const BuyCoffeeButton: FunctionComponent<BuyCoffeeButtonProps> = (props) => {
+  const {withIcon = false} = props;
+  const [title, setTitle] = useState<string>();
+
+  useEffect(() => {
+    setTitle(
+      translateString(withIcon ? 'BUY_ME_COFFEE_TITLE_SHORT' : 'BUY_ME_COFFEE_TITLE'),
+    );
+  }, [withIcon]);
+
   return (
     <Button
       onClick={() => handleLink('BUY_ME_COFFEE_LINK', {close: true, active: true})}
+      endIcon={withIcon ? <FreeBreakfast/> : null}
+      {...props}
     >
-      {translateString('BUY_ME_COFFEE_TITLE')}
+      {title}
     </Button>
+  );
+};
+
+export const BuyCoffeeIconButton: FunctionComponent<IconButtonProps> = (props) => {
+  return (
+    <IconButton
+      {...props}
+      onClick={() => handleLink('BUY_ME_COFFEE_LINK', {close: true, active: true})}
+    >
+      <FreeBreakfast/>
+    </IconButton>
   );
 };
 
