2479efcb823f6e7c2a63211d8d9d439606021beb
Peter Pshenichny <peter.pshenichny@gmail.com>
2021-04-15 22:46:06 +0300

    new button with countdown, will take user to calendar generator

---
+++ src/components/buttons/to-calendar-creator.button/to-calendar-creator.button.tsx
@@ -0,0 +1,67 @@
+import {
+  Button,
+  makeStyles,
+  Theme,
+} from '@material-ui/core';
+import { green } from '@material-ui/core/colors';
+import React, {
+  FunctionComponent,
+  useEffect,
+  useState,
+} from 'react';
+import { useHistory } from 'react-router-dom';
+import { translate } from '../../../filters/translate';
+import { handleCloseModal } from '../../modals/modals.lib';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  success: {
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[700],
+    },
+  },
+}));
+
+interface LeaveFeedbackButtonProps {
+  onClick?: () => void;
+}
+
+const ToCalendarCreatorButton: FunctionComponent<LeaveFeedbackButtonProps> = (props) => {
+  const classes = useStyles();
+  const [countdown, setCountDown] = useState(60);
+  const history = useHistory();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountDown(oldCountdown => --oldCountdown);
+    }, 1_000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      handleClick();
+    }
+  }, [countdown]);
+
+  const handleClick = () => {
+    history.replace('/export');
+    handleCloseModal();
+  };
+
+  return (
+    <Button size="small"
+            color="primary"
+            variant="contained"
+            className={classes.success}
+            onClick={handleClick}
+    >
+      {translate('TO_CALENDAR_GENERATOR_TITLE', [countdown.toString()])}
+    </Button>
+  );
+};
+
+export default ToCalendarCreatorButton;
