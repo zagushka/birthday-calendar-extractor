63564e3e906b3d48325167903095960e9a66e4b9
Peter Pshenichny <peter.pshenichny@gmail.com>
2021-04-27 01:02:11 +0300

    $13 replaced old csv export for calendars with csv export for birthdates. This decision came with idea that ics is much better for calendar creation purposes and should be used instead of csv.

---
+++ src/components/wizards/csv-data/csv-data.tsx
@@ -0,0 +1,38 @@
+import { Typography } from '@material-ui/core';
+import React, {
+  FunctionComponent,
+  useContext,
+  useMemo,
+} from 'react';
+import { CurrentStatusContext } from '../../../context/current-status.context';
+import { translate } from '../../../filters/translate';
+import { CREATE_CSV_DATA } from '../../../libs/events/types';
+import { useHandleDownload } from '../../../libs/hooks/handle-download-calendar-results.hook';
+import { STORED_BIRTHDAY } from '../../../libs/storage/storaged.types';
+import GenerateAndDownloadButton from '../../buttons/generate.button/generate.button';
+
+const CsvDataGeneratorWizard: FunctionComponent = () => {
+  const {users, isScanning} = useContext(CurrentStatusContext);
+
+  // Remove "hidden" users from the list
+  const activeUsers = useMemo(() => users.filter(u => !(u[STORED_BIRTHDAY.SETTINGS] ?? 0 & 1 << 0)), [users]);
+
+  const {startDownload} = useHandleDownload(CREATE_CSV_DATA, activeUsers);
+
+  const startGeneration = () => startDownload();
+
+  return (
+    <>
+      <Typography variant="body2">
+        {translate('SELECT_CSV_DATA_DESCRIPTION')}
+      </Typography>
+
+      <GenerateAndDownloadButton
+        disabled={isScanning}
+        onClick={startGeneration}
+      />
+    </>
+  );
+};
+
+export default CsvDataGeneratorWizard;
