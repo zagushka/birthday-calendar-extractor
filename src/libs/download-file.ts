4e731b0bc9f69c58e2456047a656ca19423079b1
Peter Pshenichny <peter.pshenichny@gmail.com>
2021-04-27 00:55:39 +0300

    new download file observable

---
+++ src/libs/download-file.ts
@@ -0,0 +1,26 @@
+import { Observable } from 'rxjs';
+
+export interface DownloadFileOptions {
+  mimeType: 'text/csv; charset=UTF-8' | 'text/calendar; charset=UTF-8' | 'application/json; charset=UTF-8';
+  filename: string;
+}
+
+export function downloadFile(calendarData: string, {mimeType, filename}: DownloadFileOptions): Observable<boolean> {
+  return new Observable((subscriber) => {
+    const url = URL.createObjectURL(
+      new Blob([calendarData], {endings: 'transparent', type: mimeType}),
+    );
+
+    chrome.downloads.download({url, filename: filename},
+      (downloadId) => {
+        if ('undefined' === typeof downloadId) {
+          return subscriber.error({message: 'FAILED TO DOWNLOAD FILE', error: chrome.runtime.lastError});
+        }
+        subscriber.next(true);
+        subscriber.complete();
+      },
+    );
+
+    return () => subscriber.complete();
+  });
+}
