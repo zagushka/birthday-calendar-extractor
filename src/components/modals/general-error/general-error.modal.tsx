87f2340e493fe02e00df44a47f544a763b70a27f
Peter <peter.pshen@getyourguide.com>
2024-11-20 19:18:20 +0100

    Removed encoded error description and copy functionality.

--- src/components/modals/general-error/general-error.modal.tsx
+++ src/components/modals/general-error/general-error.modal.tsx
@@ -3,15 +3,12 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
-  TextField,
   Typography,
 } from '@material-ui/core';
 import React, {
   FunctionComponent,
-  useRef,
 } from 'react';
 import { t } from '../../../filters/translate';
-import { translateString } from '../../../filters/translateString';
 import { ShowModalAction } from '../../../libs/events/types';
 import { DialogCloseButton } from '../../buttons/dialog-close/dialog-close';
 import {
@@ -21,21 +18,10 @@ import {
 } from '../modals.lib';
 
 const GeneralErrorModal: FunctionComponent<{ error: ShowModalAction }> = ({ error }) => {
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
-  const errorMessage = btoa(JSON.stringify(error));
-
   const handleReportBugClick = () => {
-    copyToClipboard();
     handleLinkClickAndCloseModal('REPORT_A_BUG_URL');
   };
 
-  const copyToClipboard = () => {
-    const element = textAreaRef.current;
-    element.focus();
-    element.select();
-    document.execCommand('Copy');
-    element.blur();
-  };
   return (
     <Dialog
       open
@@ -49,22 +35,6 @@ const GeneralErrorModal: FunctionComponent<{ error: ShowModalAction }> = ({ erro
         <Typography variant="body1" color="textSecondary" paragraph>
           {t(error.type)}
         </Typography>
-        <Typography variant="body1" color="textSecondary" paragraph>
-          {t('REPORT_A_BUG_DESCRIPTION')}
-        </Typography>
-        <TextField
-          inputRef={textAreaRef}
-          minRows={4}
-          maxRows={4}
-          fullWidth
-          margin="none"
-          value={translateString('REPORT_A_BUG_DETAILS_TEXTAREA', [errorMessage])}
-          multiline
-          variant="outlined"
-          InputProps={{
-            readOnly: true,
-          }}
-        />
       </DialogContent>
 
       <DialogActions>
@@ -76,7 +46,7 @@ const GeneralErrorModal: FunctionComponent<{ error: ShowModalAction }> = ({ erro
         >
           {t('REPORT_A_BUG_TITLE')}
         </Button>
-        <DialogCloseButton />
+        <DialogCloseButton/>
       </DialogActions>
 
     </Dialog>
